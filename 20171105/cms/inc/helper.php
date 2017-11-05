30f4b698a40112162334f7e8954fd7998a58c34f
Faisal Kaleem <faisal@wizcoders.com>
2017-11-05 16:36:34 +0500

    created simple form and inserted data into db

---
+++ 20171105/cms/inc/helper.php
@@ -0,0 +1,12 @@
+<?php
+function printR($record) {
+    echo '<pre>';
+    print_r($record);
+    echo '</pre>';
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
